0b5b95836723e0b9d75355ccd2ffa991c0631fac
bmansk8 <bmansk8@yahoo.com>
2019-09-24 10:24:21 -0400

    Initial commit

---
+++ age calculator/ageCalculator.js
@@ -0,0 +1,3 @@
+var age = prompt('how old are you in years?');
+age = age*365.25;
+alert('your are roughly ' + age + ' days old.')
---
+++ stalker project/stalker.js
@@ -0,0 +1,6 @@
+var firstName = prompt('whats your name?');
+var lastName = prompt('whats your last name?');
+var age = prompt('whats your age?');
+
+console.log('Your full name is ' + firstName + ' ' + lastName + '.');
+console.log('you are ' + age + ' years old.')
